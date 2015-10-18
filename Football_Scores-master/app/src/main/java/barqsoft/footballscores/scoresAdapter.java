d12ed287b5c1c77ab83029fb9f9492f5f98f516c
adeneche <adeneche@gmail.com>
2015-10-18 10:07:12 -0700

    added content descriptors

--- Football_Scores-master/app/src/main/java/barqsoft/footballscores/scoresAdapter.java
+++ Football_Scores-master/app/src/main/java/barqsoft/footballscores/scoresAdapter.java
@@ -45,16 +45,22 @@ public class scoresAdapter extends CursorAdapter
     public void bindView(View view, final Context context, Cursor cursor)
     {
         final ViewHolder mHolder = (ViewHolder) view.getTag();
-        mHolder.home_name.setText(cursor.getString(COL_HOME));
-        mHolder.away_name.setText(cursor.getString(COL_AWAY));
+        final String homeName = cursor.getString(COL_HOME);
+        mHolder.home_name.setText(homeName);
+        final String awayName = cursor.getString(COL_AWAY);
+        mHolder.away_name.setText(awayName);
         mHolder.date.setText(cursor.getString(COL_MATCHTIME));
-        mHolder.score.setText(Utilies.getScores(cursor.getInt(COL_HOME_GOALS),cursor.getInt(COL_AWAY_GOALS)));
+        final int homeGoals = cursor.getInt(COL_HOME_GOALS);
+        final int awayGoals = cursor.getInt(COL_AWAY_GOALS);
+        mHolder.score.setText(Utilies.getScores(homeGoals, awayGoals));
         mHolder.match_id = cursor.getDouble(COL_ID);
         mHolder.home_crest.setImageResource(Utilies.getTeamCrestByTeamName(
                 cursor.getString(COL_HOME)));
         mHolder.away_crest.setImageResource(Utilies.getTeamCrestByTeamName(
                 cursor.getString(COL_AWAY)
         ));
+
+        view.setContentDescription(homeName + ", " + homeGoals + ". " + awayName + ", " + awayGoals);
         //Log.v(FetchScoreTask.LOG_TAG,mHolder.home_name.getText() + " Vs. " + mHolder.away_name.getText() +" id " + String.valueOf(mHolder.match_id));
         //Log.v(FetchScoreTask.LOG_TAG,String.valueOf(detail_match_id));
         LayoutInflater vi = (LayoutInflater) context.getApplicationContext()
